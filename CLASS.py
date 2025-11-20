0a397f35080c58150c59f9450ba96ccebaa053af
fajarihasim150 <fajarihasim150@gmail.com>
2025-11-20 16:26:12 +0900

    first commit

---
+++ CLASS.py
@@ -0,0 +1,15 @@
+class Mahasiswa:
+    def __init__(self, nama, nim):
+        self.nama = nama
+        self.nim = nim
+
+    def perkenalan(self):
+        print(f"Hai! Nama saya {self.nama}, NIM saya {self.nim}")
+
+m1 = Mahasiswa("fajri", "121055520121150")
+m2 = Mahasiswa("firman", "121055520121048")
+
+m1.perkenalan()
+m2.perkenalan()
+
+print("NIM Budi:", m1.nim)
